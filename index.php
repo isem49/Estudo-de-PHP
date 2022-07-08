b70a9558da517d79a8235f9f4b83c2204ffbca37
Júnior <56940605+isem49@users.noreply.github.com>
2022-07-08 16:34:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+
+//Imprimndo texto
+echo "Testando meu primeiro programa em php!!! <br><br>";
+
+//Declarando váriaveis em php
+
+//String
+$nome = "Junior";
+
+//Inteiro
+$idade = 19;
+
+//Double ou floats
+$testeDouble = 1.5;
+
+//Boolean
+$situacao = true;
+
+//imprimindo váriaveis
+
+echo "Nome: $nome <br><br>";
+
+echo "Idade: $idade <br><br>";
+
+echo "Double: $testeDouble <br><br>";
+
+//OBS: Caso você utilize Aspas Simples (' ') ele reconhece a váriavel como texto, exemplo:
+
+echo 'Nome: $nome <br><br>';
+
+echo 'Idade: $idade <br><br>';
+
+echo 'Double: $testeDouble <br><br>';
+
+//Concatenação
+
+echo "Meu nome é $nome, " . "tenho $idade anos" . " e gosto de estudar programação" . "<br><br>";
+
+//Calculos
+
+$quantidadeP = 50;
+
+echo "Quantidade inicial " .  $quantidadeP . "<br><br>";
+
+$quantidadeP = $quantidadeP - 10;
+
+echo "Quantidade após subtrair um valor " . $quantidadeP . "<br><br>";
+
+//Calculos com double
+
+$saldo = 15000000.5;
+$deposito = 30.55;
+
+echo "Saldo atual: $saldo <br><br>";
+echo "Valor a ser depositsado: $deposito <br><br>";
+
+$saldo = $saldo + $deposito;
+
+echo "Saldo depois do deposito: $saldo <br><br>";
+
+//Escolhendo casas decimais e formatação para real
+
+echo "Saldo: " . number_format($saldo, 3) . "<br><br>";
+
+echo "Saldo: " . number_format($saldo, 3, ".",   ".",) . "<br><br>";
+
+//testando Boolean
+
+if ($situacao) {
+
+    echo "<span style= 'color: green;'> A situação é verdadeira<br><br></span>";
+} else {
+
+    echo "<span style= 'color: red'; >A situação é falsa </span> <br><br>";
+}
+
+$situacao = false;
+
+if ($situacao) {
+
+    echo "<span style= 'color: green;'> A situação é verdadeira</span>";
+} else {
+
+    echo "<span style= 'color: red'; >A situação é falsa </span>";
+}
+
+?>
