05030a1b832535d193e568cc53f20e46e59a647f
Júnior <56940605+isem49@users.noreply.github.com>
2022-07-18 15:54:08 -0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "cicero";
+$port = 3306;
+
+//Conexao com a porta
+$conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+ 
+?>
